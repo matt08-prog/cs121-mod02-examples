89f70a552b8db8db4868778eba57d470a13b8829
BoiseState Student <student@u.boisestate.edu>
2022-01-28 17:41:42 -0700

    fixed_ConversionFun

--- Expressions/ConversionFun.java
+++ Expressions/ConversionFun.java
@@ -4,8 +4,13 @@ public class ConversionFun {
         double radiusCubed = 1.0;
         double volume1 = 4 / 3 * PI * radiusCubed;
         double volume2 = PI * radiusCubed * 4 / 3;
+        double volume3 = PI * radiusCubed * (4 / 3);  
+        double volume4 = 4 / (3 * PI) * radiusCubed;  
+
 
         System.out.println("Volume 1: " + volume1);
         System.out.println("Volume 2: " + volume2);
+        System.out.println("Volume 3: " + volume3);
+        System.out.println("Volume 4: " + volume4);
     }
 }
